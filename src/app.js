091af06aacc25617aba905461a63d58206487dad
Ramses Cevallos <cevallosramses@gmail.com>
2022-06-02 20:13:10 +0000

    making the output like the example

--- src/app.js
+++ src/app.js
@@ -1,29 +1,28 @@
 function domainGenerator() {
-  let pronoun = ["the"];
-  let adj = ["destroyer", "suicide"];
-  let noun = ["squad", "netflownet", "audio"];
-  let extentsions = ["com", "net", "io"];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
   let counter = 0;
 
-  pronoun.forEach(elePronoun => {
-    adj.forEach(eleAdj => {
-      noun.forEach(eleNoun => {
-        extentsions.forEach(eleExtensions => {
-          if (eleNoun.endsWith(eleExtensions)) {
-            let aux = eleNoun.slice(0, -eleExtensions.length);
-            counter++;
-            console.log(
-              `${counter}-> ${elePronoun}${eleAdj}${aux}.${eleExtensions}`
-            );
-          } else {
-            counter++;
-            console.log(
-              `${counter}-> ${elePronoun}${eleAdj}${eleNoun}.${eleExtensions}`
-            );
-          }
-        });
-      });
-    });
-  });
+  for (let i = 0; i < pronoun.length; i++) {   
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) { 
+        counter++;
+        console.log(counter, pronoun[i] + adj[j] + noun[k] + ".com");       
+      }
+      let aux = pronoun[0];
+        pronoun[0] = pronoun[1];
+        pronoun[1] = aux;
+      let aux2 = adj[0];
+        adj[0] = adj[1];
+        adj[1] = aux2;
+    }
+    let aux = pronoun[0];
+        pronoun[0] = pronoun[1];
+        pronoun[1] = aux;
+      let aux2 = adj[0];
+        adj[0] = adj[1];
+        adj[1] = aux2;
+  }
 }
 domainGenerator();
